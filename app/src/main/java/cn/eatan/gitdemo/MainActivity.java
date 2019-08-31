25f880307fdc3de6132a292e74e328f63b6d0f8f
eatan <eatan96@sina.com>
2019-08-31 22:32:14 +0800

    B:修改为Button

--- app/src/main/java/cn/eatan/gitdemo/MainActivity.java
+++ app/src/main/java/cn/eatan/gitdemo/MainActivity.java
@@ -3,19 +3,26 @@ package cn.eatan.gitdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText editText;
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editText = findViewById(R.id.edit_text);
+        button = findViewById(R.id.button);
 
-        Toast.makeText(this, editText.getText().toString(), Toast.LENGTH_SHORT).show();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
