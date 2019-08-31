c3401092543658a24e18c21a45dcadc5c735b201
eatan <eatan96@sina.com>
2019-08-31 22:36:47 +0800

    A：增加了一个Button

--- app/src/main/java/cn/eatan/gitdemo/MainActivity.java
+++ app/src/main/java/cn/eatan/gitdemo/MainActivity.java
@@ -3,18 +3,29 @@ package cn.eatan.gitdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText editText;
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         editText = findViewById(R.id.edit_text);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
 
         Toast.makeText(this, editText.getText().toString(), Toast.LENGTH_SHORT).show();
     }
