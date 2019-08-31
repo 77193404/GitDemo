93b7086db1929314281d975218f8f5cc401020a3
eatan <eatan96@sina.com>
2019-08-31 22:25:33 +0800

    A:修改为EditText

--- app/src/main/java/cn/eatan/gitdemo/MainActivity.java
+++ app/src/main/java/cn/eatan/gitdemo/MainActivity.java
@@ -3,12 +3,19 @@ package cn.eatan.gitdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.edit_text);
+
+        Toast.makeText(this, editText.getText().toString(), Toast.LENGTH_SHORT).show();
     }
 }
